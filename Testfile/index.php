64665bd6219a9d99dfc4460898a850275c82ddd2
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-04-17 13:26:15 +0800

    Upload image Testing
    shesh

---
+++ Testfile/index.php
@@ -0,0 +1,43 @@
+<?php
+
+require 'config.php';
+
+?>
+<html>
+<head> </head>
+<body>
+<table border = 1 cellspacing=0 cellpadding= 10>
+    <tr>
+        <td>#</td>
+        <td>Name</td>
+        <td>Image</td>
+    </tr>
+
+    <?php
+    $i = 1;
+
+
+
+    
+    $rows = mysqli_query($conn, "SELECT FROM tb_images");
+    foreach($rows as $row) :
+    ?>
+    <tr>
+    <td> <?php echo $i++; ?> </td>
+    
+<?php echo $row["name"];?> <td>
+
+    <td style="display: flex; align-items: center; gap: 10px"> <?php
+foreach(json_decode($row["image"]) as $image) :
+?>
+<img src="uploads/<?php echo $image; ?>" width=200>
+<?php endforeach; ?>
+</td>
+</tr>
+<?php endforeach; ?>
+</table>
+<br>
+<a href="upload.php">Upload Image</a>
+</body>
+</html>
+I
