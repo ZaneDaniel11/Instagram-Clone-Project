43765bf0089ec30c0773233dbe60d848880872e8
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-04-22 14:52:30 +0800

    shesh
    21

--- CommentFile/index.php
+++ CommentFile/index.php
@@ -20,8 +20,7 @@ if (isset($_SESSION['status'])) {
             <h4>This is a Blog Heading</h4>
           </div>
           <div class="card-body">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It
-            <hr>
+            shesh
             <div class="main-comment">
               <div id="error_status"></div>
               <textarea class="comment_text form-control" id="comment_textbox" rows="3"></textarea>
