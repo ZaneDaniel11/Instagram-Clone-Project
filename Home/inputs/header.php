13bc212f60236f66f15ea5ba8d9021ea2f6e104b
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-04-27 10:45:17 +0800

    psuhh

---
+++ Home/inputs/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instagram Post Layout</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" href="../style/styles.css">
+    <link rel="stylesheet" href="../style/style2.css">
+
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <aside class="sidebar">
+        <div class="SiteLogoContainer">
+            <img src="../style/images/instagramLogoLetter.png" alt="Instagram Logo" class="imgcontainer">
+        </div>
+        <nav class="navigation">
+            <div class="icon-bar">
+                <a href="#" class="nav-link"><i class="fas fa-home"></i> Home</a>
+                <a href="#" class="nav-link"><i class="fas fa-search"></i> Search</a>
+                <a href="#" class="nav-link"><i class="fas fa-compass"></i> Explore</a>
+                <a href="#" class="nav-link"><i class="fas fa-film"></i> Reels</a>
+                <a href="#" class="nav-link"><i class="fas fa-paper-plane"></i> Messages</a>
+                <a href="#" class="nav-link"><i class="fas fa-bell"></i> Notifications</a>
+                <a href="#" class="nav-link"><i class="fas fa-plus-square"></i> Create</a>
+                <a href="#" class="nav-link"><i class="fas fa-user"></i> Profile</a>
+                <a href="">Logout</a>
+            </div>
+
+            </div>
+            <div class="bnav">
+                <div class="thread">
+                    <a href="#" class="nav-link"><i class="fas fa-comment-dots"></i> Threads</a>
+                </div>
+                <div class="more">
+                    <a href="#" class="nav-link"><i class="fas fa-ellipsis-h"></i> More</a>
+    </aside>
