0f2aca324d37ba0390badf6d85d12f3d8f918992
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-05-02 12:22:06 +0800

    yeah Final
    shesh

--- Home/inputs/header.php
+++ Home/inputs/header.php
@@ -23,6 +23,15 @@
 </head>
 
 <body>
+<?php
+session_start();
+
+if (!isset($_SESSION['auth_user_id'])) {
+
+    header("Location: login.php");
+
+}
+?>
     <aside class="sidebar">
         <div class="SiteLogoContainer">
             <img src="./lib/style/images/instagramLogoLetter.png" alt="Instagram Logo" class="imgcontainer">
