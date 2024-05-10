cbe7f13bb22c7318a9ba51073472140951f3579c
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-05-10 17:39:50 +0800

    added

--- Home/inputs/footer.php
+++ Home/inputs/footer.php
@@ -1,40 +1,39 @@
-
 <style>
-    .followers_container
-    {
+    .followers_container {
         position: absolute;
         right: 0px;
         top: 0;
-   
+
         width: 300px;
         margin-top: 50px;
     }
-    .user-follow-con
-    {
+
+    .user-follow-con {
         display: flex;
         justify-content: space-between;
         margin-right: 20px;
         align-self: center;
         margin-top: 30px;
     }
-    .follow
-    {
+
+    .follow {
         border-radius: 20px;
         width: 100px;
         border-style: none;
     }
 </style>
 <div class="followers_container">
-    
-</div>
 
+</div>
+<script src="https://cdn.jsdelivr.net/gh/fontawsome/js/icons@5.0.3.js"></script>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-<script src="./js/Content.js"></script> 
+<script src="./js/Content.js"></script>
 <script src="./js/user.js"></script>
-<script src="./js/content_edit.js"></script> 
+<script src="./js/content_edit.js"></script>
 <script src="./js/Comment.js"></script>
 
 
 </body>
+
 </html>
--- Home/inputs/header.php
+++ Home/inputs/header.php
@@ -10,28 +10,27 @@
     <link rel="stylesheet" href="./lib/style/style2.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    
+    <script src="https://cdn.jsdelivr.net/gh/fontawsome/js/icons@5.0.3.js"></script>
+
 
     <style>
-            .nav-link
-            {
-                display: flex;
-                margin-bottom: 28px;
-                margin-top: 0px;
-            }
+        .nav-link {
+            display: flex;
+            margin-bottom: 28px;
+            margin-top: 0px;
+        }
     </style>
 </head>
 
 <body>
-<?php
-session_start();
+    <?php
+    session_start();
 
-if (!isset($_SESSION['auth_user_id'])) {
+    if (!isset($_SESSION['auth_user_id'])) {
 
-    header("Location: login.php");
-
-}
-?>
+        header("Location: login.php");
+    }
+    ?>
     <aside class="sidebar">
         <div class="SiteLogoContainer">
             <img src="./lib/style/images/instagramLogoLetter.png" alt="Instagram Logo" class="imgcontainer">
@@ -44,7 +43,7 @@ if (!isset($_SESSION['auth_user_id'])) {
                 <a href="#" class="nav-link"><i class="fas fa-film"></i> Reels</a>
                 <a href="#" class="nav-link"><i class="fas fa-paper-plane"></i> Messages</a>
                 <a href="#" class="nav-link"><i class="fas fa-bell"></i> Notifications</a>
-                <a class="nav-link" ata-bs-toggle="modal" data-bs-target="#exampleModal"><i class=" fas fa-plus-square"data-bs-toggle="modal" data-bs-target="#exampleModal"></i> Create</a>
+                <a class="nav-link" ata-bs-toggle="modal" data-bs-target="#exampleModal"><i class=" fas fa-plus-square" data-bs-toggle="modal" data-bs-target="#exampleModal"></i> Create</a>
                 <a href="#" class="nav-link"><i class="fas fa-user"></i> <?php if (isset($_SESSION['authuser_name'])) {
                                                                                 echo $_SESSION['authuser_name'];
                                                                             } else {
@@ -53,7 +52,7 @@ if (!isset($_SESSION['auth_user_id'])) {
                 <a href="./Code/logout.php">Logout</a>
             </div>
 
-        
+
             <div class="bnav">
                 <div class="thread">
                     <a href="#" class="nav-link"><i class="fas fa-comment-dots"></i> Threads</a>
@@ -64,34 +63,34 @@ if (!isset($_SESSION['auth_user_id'])) {
             </div>
         </nav>
     </aside>
-</div>
-<!-- Button trigger modal -->
-
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-    <form method="POST" action="./Code/content.php" enctype="multipart/form-data">
-      <div class="modal-body">
-          <div class="mb-3">
-              <label for="text" class="form-label">Content Name</label>
-              <input type="text" class="form-control" name="content" id="content">
-          </div>
-          <div class="mb-3">
-              <div class="input-group">
-                  <input type="file" class="form-control fileImg" name="fileImg[]" accept=".jpg, .jpeg, .png" aria-describedby="inputGroupFileAddon04" aria-label="Upload" required multiple>
-              </div>
-          </div>
-          <button type="submit" name="submit" class="btn btn-primary upload-button">Submit</button>
-      </div>
-    </form>
-      </div>
     </div>
-  </div>
-</div>  
+    <!-- Button trigger modal -->
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form method="POST" action="./Code/content.php" enctype="multipart/form-data">
+                        <div class="modal-body">
+                            <div class="mb-3">
+                                <label for="text" class="form-label">Content Name</label>
+                                <input type="text" class="form-control" name="content" id="content">
+                            </div>
+                            <div class="mb-3">
+                                <div class="input-group">
+                                    <input type="file" class="form-control fileImg" name="fileImg[]" accept=".jpg, .jpeg, .png" aria-describedby="inputGroupFileAddon04" aria-label="Upload" required multiple>
+                                </div>
+                            </div>
+                            <button type="submit" name="submit" class="btn btn-primary upload-button">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
