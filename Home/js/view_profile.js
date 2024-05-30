b1d5baf39a41331b908590d5d257513177d0f547
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-05-30 22:42:00 +0800

    view profile

--- Home/js/view_profile.js
+++ Home/js/view_profile.js
@@ -94,10 +94,10 @@ $(document).ready(function() {
                         response.forEach(function(value) {
                             let contentHTML = '<div class="profile">\
 			<div class="profile-image">\
-				<img src="https://images.unsplash.com/photo-1513721032312-6a18a42c8763?w=152&h=152&fit=crop&crop=faces" alt="">\
+				<img src="https://vectorified.com/images/no-profile-picture-icon-10.jpg" alt="">\
 			</div>\
 			<div class="profile-user-settings">\
-				<h1 class="profile-user-name">janedoe_</h1>\
+				<h1 class="profile-user-name">'+value.content.user_name+'</h1>\
 				<button class="btn profile-edit-btn">Edit Profile</button>\
 				<button class="btn profile-settings-btn" aria-label="profile settings"><i class="fas fa-cog" aria-hidden="true"></i></button>\
 			</div>\
